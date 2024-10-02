c438d53938479204a313cc63b46905fcef036ed5
wellingto198 <52017348+wellingto198@users.noreply.github.com>
2024-10-02 13:41:27 -0300

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -42,13 +42,17 @@ module.exports = {
   protectedRoutes: ['Drive/Ultra-secret%20Valeyard', 'O que você ta fazendo aqui?'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
-  "email: 'mailto:Wellingto198@yfb0q.onmicrosoft.com',"
+  email: '',
 
-  // [OPTIONAL] This is an array of names and links for setting your social information and links.
-  // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
-  // you provide. See the documentation for details.
-links: [{name: 'Telegram',
-      link: 'https://t.me/DWWebTV',},],
+ // [OPTIONAL] This is an array of names and links for setting your social information and links.
+// In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
+// you provide. See the documentation for details.
+links: [
+  {
+    name: 'Telegram',
+    link: 'https://t.me/DWWebTV',
+  }
+],
 
   // This is a day.js-style datetime format string to format datetimes in the app. Ref to
   // https://day.js.org/docs/en/display/format for detailed specification. The default value is ISO 8601 full datetime
