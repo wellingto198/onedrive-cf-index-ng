677164cfbed6fc2a16f73c53658fb0462118e9c5
wellingto198 <52017348+wellingto198@users.noreply.github.com>
2024-10-02 10:24:36 -0300

    Update site.config.js
    My confg

--- config/site.config.js
+++ config/site.config.js
@@ -14,7 +14,7 @@ module.exports = {
   icon: '/icons/128.png',
 
   // The name of your website. Present alongside your icon.
-  title: "lyc8503's OneDrive",
+  title: "DW WEB TV",
 
   // The folder that you are to share publicly with onedrive-cf-index-ng. Use '/' if you want to share your root folder.
   baseDirectory: process.env.BASE_DIRECTORY || '/',
@@ -42,12 +42,12 @@ module.exports = {
   protectedRoutes: ['/Private', '/Demo/😎Another Private Folder Password 123'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
-  email: 'mailto:me@lyc8503.net',
+  //email: 'mailto:Wellingto198@yfb0q.onmicrosoft.com',
 
   // [OPTIONAL] This is an array of names and links for setting your social information and links.
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
   // you provide. See the documentation for details.
-  links: [
+ // links: [
     {
       name: 'GitHub',
       link: 'https://github.com/lyc8503',
