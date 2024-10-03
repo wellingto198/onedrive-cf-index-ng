f4c2cbf339409ccdab0db1fa9bac2c4d34236f96
wellingto198 <52017348+wellingto198@users.noreply.github.com>
2024-10-03 07:00:38 -0300

    Update site.config.js
    Lock folder

--- config/site.config.js
+++ config/site.config.js
@@ -39,7 +39,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['Drive/Ultra-secret Valeyard', 'O que você ta fazendo aqui?'],
+  protectedRoutes: ['/Drive/Ultra-secret Valeyard', 'O que você ta fazendo aqui?'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: '',
