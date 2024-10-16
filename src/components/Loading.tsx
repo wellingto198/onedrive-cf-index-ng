4b9a54e3bf3ceb5f362d8e01254bd8abe43c7897
wellingto198 <52017348+wellingto198@users.noreply.github.com>
2024-10-16 11:29:05 -0300

    Update Loading.tsx

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,16 +1,23 @@
-const Loading: React.FC<{ loadingText: string }> = ({ loadingText }) => {
+const Carregando: React.FC<{ textoCarregando: string }> = ({ textoCarregando }) => {
   return (
     <div className="flex items-center justify-center space-x-1 rounded py-32 dark:text-white">
-      <LoadingIcon className="-ml-1 mr-3 h-5 w-5 animate-spin" />
-      <div>{loadingText}</div>
+      <IconeCarregando className="-ml-1 mr-3 h-5 w-5 animate-spin" />
+      <div>{textoCarregando}</div>
     </div>
   )
 }
 
-// As there is no CSS-in-JS styling system, pass class list to override styles
-export const LoadingIcon: React.FC<{ className?: string }> = ({ className }) => {
+// Atualizado com o className opcional e aria-label para acessibilidade
+export const IconeCarregando: React.FC<{ className?: string }> = ({ className }) => {
   return (
-    <svg className={className} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+    <svg
+      className={className}
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      aria-label="Carregando..." // Adicionado aria-label para acessibilidade
+      role="status" // Indica que é um indicador de carregamento
+    >
       <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" />
       <path
         className="opacity-75"
@@ -21,4 +28,4 @@ export const LoadingIcon: React.FC<{ className?: string }> = ({ className }) => 
   )
 }
 
-export default Loading
+export default Carregando
