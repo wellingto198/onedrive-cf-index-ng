7a562dbb98e2d1d992af1eafe074a1927f1c61fb
wellingto198 <52017348+wellingto198@users.noreply.github.com>
2024-10-16 08:33:37 -0300

    Update site.config.js
    Data

--- config/site.config.js
+++ config/site.config.js
@@ -57,5 +57,5 @@ links: [
   // This is a day.js-style datetime format string to format datetimes in the app. Ref to
   // https://day.js.org/docs/en/display/format for detailed specification. The default value is ISO 8601 full datetime
   // without timezone and replacing T with space.
-  datetimeFormat: 'YYYY-MM-DD HH:mm:ss',
+  datetimeFormat: 'DD-MM-YY',
 }
