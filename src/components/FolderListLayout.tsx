b1560bf07b7cb27abfa0ae7773d20bfd1ff24fe6
wellingto198 <52017348+wellingto198@users.noreply.github.com>
2024-10-16 20:25:51 -0300

    Update FolderListLayout.tsx

--- src/components/FolderListLayout.tsx
+++ src/components/FolderListLayout.tsx
@@ -11,26 +11,26 @@ import { humanFileSize, formatModifiedDateTime } from '../utils/fileDetails'
 import { Downloading, Checkbox, ChildIcon, ChildName } from './FileListing'
 import { getStoredToken } from '../utils/protectedRouteHandler'
 
-const ItemListaArquivo: FC<{ conteudoArquivo: OdFolderChildren }> = ({ conteudoArquivo: c }) => {
+const FileItem: FC<{ fileContent: OdFolderChildren }> = ({ fileContent: f }) => {
   return (
     <div className="grid cursor-pointer grid-cols-10 items-center space-x-2 px-3 py-2.5">
-      <div className="col-span-10 flex items-center space-x-2 truncate md:col-span-6" title={c.name}>
+      <div className="col-span-10 flex items-center space-x-2 truncate md:col-span-6" title={f.name}>
         <div className="w-5 flex-shrink-0 text-center">
-          <ChildIcon child={c} />
+          <ChildIcon child={f} />
         </div>
-        <ChildName name={c.name} folder={Boolean(c.folder)} />
+        <ChildName name={f.name} folder={Boolean(f.folder)} />
       </div>
       <div className="col-span-3 hidden flex-shrink-0 font-mono text-sm text-gray-700 dark:text-gray-500 md:block">
-        {formatModifiedDateTime(c.lastModifiedDateTime)}
+        {formatModifiedDateTime(f.lastModifiedDateTime)}
       </div>
       <div className="col-span-1 hidden flex-shrink-0 truncate font-mono text-sm text-gray-700 dark:text-gray-500 md:block">
-        {humanFileSize(c.size)}
+        {humanFileSize(f.size)}
       </div>
     </div>
   )
 }
 
-const LayoutListaPasta = ({
+const FolderListLayout = ({
   path,
   folderChildren,
   selected,
@@ -45,53 +45,53 @@ const LayoutListaPasta = ({
   toast,
 }) => {
   const clipboard = useClipboard()
-  const tokenHash = getStoredToken(caminho)
+  const tokenHash = getStoredToken(path)
 
-  // Obter caminho do item pelo nome
-  const obterCaminhoItem = (nome: string) => `${caminho === '/' ? '' : caminho}/${encodeURIComponent(nome)}`
+  // Get path for an item by name
+  const getItemPath = (name: string) => `${path === '/' ? '' : path}/${encodeURIComponent(name)}`
 
   return (
     <div className="rounded bg-white shadow-sm dark:bg-gray-900 dark:text-gray-100">
       <div className="grid grid-cols-12 items-center space-x-2 border-b border-gray-900/10 px-3 dark:border-gray-500/30">
         <div className="col-span-12 py-2 text-xs font-bold uppercase tracking-widest text-gray-600 dark:text-gray-300 md:col-span-6">
-          {'Nome'}
+          {'Name'}
         </div>
         <div className="col-span-3 hidden text-xs font-bold uppercase tracking-widest text-gray-600 dark:text-gray-300 md:block">
-          {'Última Modificação'}
+          {'Last Modified'}
         </div>
         <div className="hidden text-xs font-bold uppercase tracking-widest text-gray-600 dark:text-gray-300 md:block">
-          {'Tamanho'}
+          {'Size'}
         </div>
         <div className="hidden text-xs font-bold uppercase tracking-widest text-gray-600 dark:text-gray-300 md:block">
-          {'Ações'}
+          {'Actions'}
         </div>
         <div className="hidden text-xs font-bold uppercase tracking-widest text-gray-600 dark:text-gray-300 md:block">
           <div className="hidden p-1.5 text-gray-700 dark:text-gray-400 md:flex">
             <Checkbox
-              checked={totalSelecionados}
-              onChange={alternarTotalSelecionados}
+              checked={totalSelected}
+              onChange={toggleTotalSelected}
               indeterminate={true}
-              title={'Selecionar arquivos'}
+              title={'Select files'}
             />
             <button
-              title={'Copiar permalink dos arquivos selecionados'}
+              title={'Copy permalink of selected files'}
               className="cursor-pointer rounded p-1.5 hover:bg-gray-300 disabled:cursor-not-allowed disabled:text-gray-400 disabled:hover:bg-white dark:hover:bg-gray-600 disabled:dark:text-gray-600 disabled:hover:dark:bg-gray-900"
-              disabled={totalSelecionados === 0}
+              disabled={totalSelected === 0}
               onClick={() => {
-                clipboard.copy(manipularPermalinkSelecionados(getBaseUrl()))
-                toast.success('Permalink dos arquivos selecionados copiado.')
+                clipboard.copy(handleSelectedPermalink(getBaseUrl()))
+                toast.success('Permalink of selected files copied.')
               }}
             >
               <FontAwesomeIcon icon={['far', 'copy']} size="lg" />
             </button>
-            {gerandoTotal ? (
-              <Downloading title={'Baixando arquivos selecionados, recarregue a página para cancelar'} style="p-1.5" />
+            {totalGenerating ? (
+              <Downloading title={'Downloading selected files, refresh the page to cancel'} style="p-1.5" />
             ) : (
               <button
-                title={'Baixar arquivos selecionados'}
+                title={'Download selected files'}
                 className="cursor-pointer rounded p-1.5 hover:bg-gray-300 disabled:cursor-not-allowed disabled:text-gray-400 disabled:hover:bg-white dark:hover:bg-gray-600 disabled:dark:text-gray-600 disabled:hover:dark:bg-gray-900"
-                disabled={totalSelecionados === 0}
-                onClick={manipularDownloadSelecionados}
+                disabled={totalSelected === 0}
+                onClick={handleSelectedDownload}
               >
                 <FontAwesomeIcon icon={['far', 'arrow-alt-circle-down']} size="lg" />
               </button>
@@ -100,40 +100,40 @@ const LayoutListaPasta = ({
         </div>
       </div>
 
-      {filhosPasta.map((c: OdFolderChildren) => (
+      {folderChildren.map((f: OdFolderChildren) => (
         <div
           className="grid grid-cols-12 transition-all duration-100 hover:bg-gray-100 dark:hover:bg-gray-850"
-          key={c.id}
+          key={f.id}
         >
           <Link
-            href={`${caminho === '/' ? '' : caminho}/${encodeURIComponent(c.name)}`}
+            href={`${path === '/' ? '' : path}/${encodeURIComponent(f.name)}`}
             passHref
             className="col-span-12 md:col-span-10"
           >
-            <ItemListaArquivo conteudoArquivo={c} />
+            <FileItem fileContent={f} />
           </Link>
 
-          {c.folder ? (
+          {f.folder ? (
             <div className="hidden p-1.5 text-gray-700 dark:text-gray-400 md:flex">
               <span
-                title={'Copiar permalink da pasta'}
+                title={'Copy folder permalink'}
                 className="cursor-pointer rounded px-1.5 py-1 hover:bg-gray-300 dark:hover:bg-gray-600"
                 onClick={() => {
-                  clipboard.copy(`${getBaseUrl()}${`${caminho === '/' ? '' : caminho}/${encodeURIComponent(c.name)}`}`)
-                  toast('Permalink da pasta copiado.', { icon: '👌' })
+                  clipboard.copy(`${getBaseUrl()}${`${path === '/' ? '' : path}/${encodeURIComponent(f.name)}`}`)
+                  toast('Folder permalink copied.', { icon: '👌' })
                 }}
               >
                 <FontAwesomeIcon icon={['far', 'copy']} />
               </span>
-              {gerandoPasta[c.id] ? (
-                <Downloading title={'Baixando pasta, recarregue a página para cancelar'} style="px-1.5 py-1" />
+              {folderGenerating[f.id] ? (
+                <Downloading title={'Downloading folder, refresh the page to cancel'} style="px-1.5 py-1" />
               ) : (
                 <span
-                  title={'Baixar pasta'}
+                  title={'Download folder'}
                   className="cursor-pointer rounded px-1.5 py-1 hover:bg-gray-300 dark:hover:bg-gray-600"
                   onClick={() => {
-                    const p = `${caminho === '/' ? '' : caminho}/${encodeURIComponent(c.name)}`
-                    manipularDownloadPasta(p, c.id, c.name)()
+                    const p = `${path === '/' ? '' : path}/${encodeURIComponent(f.name)}`
+                    handleFolderDownload(p, f.id, f.name)()
                   }}
                 >
                   <FontAwesomeIcon icon={['far', 'arrow-alt-circle-down']} />
@@ -143,32 +143,32 @@ const LayoutListaPasta = ({
           ) : (
             <div className="hidden p-1.5 text-gray-700 dark:text-gray-400 md:flex">
               <span
-                title={'Copiar permalink do arquivo bruto'}
+                title={'Copy raw file permalink'}
                 className="cursor-pointer rounded px-1.5 py-1 hover:bg-gray-300 dark:hover:bg-gray-600"
                 onClick={() => {
                   clipboard.copy(
-                    `${getBaseUrl()}/api/raw?path=${obterCaminhoItem(c.name)}${tokenHash ? `&odpt=${tokenHash}` : ''}`
+                    `${getBaseUrl()}/api/raw?path=${getItemPath(f.name)}${tokenHash ? `&odpt=${tokenHash}` : ''}`
                   )
-                  toast.success('Permalink do arquivo bruto copiado.')
+                  toast.success('Raw file permalink copied.')
                 }}
               >
                 <FontAwesomeIcon icon={['far', 'copy']} />
               </span>
               <a
-                title={'Baixar arquivo'}
+                title={'Download file'}
                 className="cursor-pointer rounded px-1.5 py-1 hover:bg-gray-300 dark:hover:bg-gray-600"
-                href={`/api/raw?path=${obterCaminhoItem(c.name)}${tokenHash ? `&odpt=${tokenHash}` : ''}`}
+                href={`/api/raw?path=${getItemPath(f.name)}${tokenHash ? `&odpt=${tokenHash}` : ''}`}
               >
                 <FontAwesomeIcon icon={['far', 'arrow-alt-circle-down']} />
               </a>
             </div>
           )}
           <div className="hidden p-1.5 text-gray-700 dark:text-gray-400 md:flex">
-            {!c.folder && !(c.name === '.password') && (
+            {!f.folder && !(f.name === '.password') && (
               <Checkbox
-                checked={selecionados[c.id] ? 2 : 0}
-                onChange={() => alternarItemSelecionado(c.id)}
-                title={'Selecionar arquivo'}
+                checked={selected[f.id] ? 2 : 0}
+                onChange={() => toggleItemSelected(f.id)}
+                title={'Select file'}
               />
             )}
           </div>
@@ -178,4 +178,4 @@ const LayoutListaPasta = ({
   )
 }
 
-export default LayoutListaPasta
+export default FolderListLayout
