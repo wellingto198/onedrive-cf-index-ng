8d426922a7757dc52e988a58ee0d46f0567fad98
wellingto198 <52017348+wellingto198@users.noreply.github.com>
2024-10-17 18:49:29 -0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,8 +4,10 @@ const createFooterMarkup = () => {
   return {
     __html: `
       ${config.footer}
-      <script id="_wauv9i">var _wau = _wau || []; _wau.push(["small", "6mzocwh7kd", "v9i"]);\u003C/script>
-      <script async src="//waust.at/s.js">\u003C/script>
+      <div style="position: absolute; right: 10px;">
+        <script id="_wauv9i">var _wau = _wau || []; _wau.push(["small", "6mzocwh7kd", "v9i"]);\u003C/script>
+        <script async src="//waust.at/s.js">\u003C/script>
+      </div>
     `,
   }
 }
@@ -13,7 +15,7 @@ const createFooterMarkup = () => {
 const Footer = () => {
   return (
     <div
-      className="w-full border-t border-gray-900/10 p-4 text-center text-xs font-medium text-gray-400 dark:border-gray-500/30"
+      className="relative w-full border-t border-gray-900/10 p-4 text-center text-xs font-medium text-gray-400 dark:border-gray-500/30"
       dangerouslySetInnerHTML={createFooterMarkup()}
     ></div>
   )
