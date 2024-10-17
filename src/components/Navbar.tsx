4af06f32c4446c4c18dc2e895aca7c7d27db4176
wellingto198 <52017348+wellingto198@users.noreply.github.com>
2024-10-17 09:11:19 -0300

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -72,7 +72,7 @@ const Navbar = () => {
           >
             <div className="flex items-center space-x-2">
               <FontAwesomeIcon className="h-4 w-4" icon="search" />
-              <span className="truncate text-sm font-medium">{'Search ...'}</span>
+              <span className="truncate text-sm font-medium">{'Pesquisar ...'}</span>
             </div>
 
             <div className="hidden items-center space-x-1 md:flex">
@@ -100,7 +100,7 @@ const Navbar = () => {
           {siteConfig.email && (
             <a href={siteConfig.email} className="flex items-center space-x-2 hover:opacity-80 dark:text-white">
               <FontAwesomeIcon icon={['far', 'envelope']} />
-              <span className="hidden text-sm font-medium md:inline-block">{'Email'}</span>
+              <span className="hidden text-sm font-medium md:inline-block">{'E-mail'}</span>
             </a>
           )}
 
@@ -109,7 +109,7 @@ const Navbar = () => {
               className="flex items-center space-x-2 hover:opacity-80 dark:text-white"
               onClick={() => setIsOpen(true)}
             >
-              <span className="hidden text-sm font-medium md:inline-block">{'Logout'}</span>
+              <span className="hidden text-sm font-medium md:inline-block">{'Sair'}</span>
               <FontAwesomeIcon icon="sign-out-alt" />
             </button>
           )}
@@ -131,7 +131,6 @@ const Navbar = () => {
               <Dialog.Overlay className="fixed inset-0 bg-gray-50 dark:bg-gray-800" />
             </Transition.Child>
 
-            {/* This element is to trick the browser into centering the modal contents. */}
             <span className="inline-block h-screen align-middle" aria-hidden="true">
               &#8203;
             </span>
@@ -146,12 +145,12 @@ const Navbar = () => {
             >
               <div className="my-8 inline-block w-full max-w-md transform overflow-hidden rounded-lg bg-white p-6 text-left align-middle transition-all dark:bg-gray-900">
                 <Dialog.Title className="text-lg font-bold text-gray-900 dark:text-gray-100">
-                  {'Clear all tokens?'}
+                  {'Limpar todos os tokens?'}
                 </Dialog.Title>
                 <div className="mt-2">
                   <p className="text-sm text-gray-500">
-                    {'These tokens are used to authenticate yourself into password protected folders, ' +
-                      'clearing them means that you will need to re-enter the passwords again.'}
+                    {'Esses tokens são usados para autenticar você em pastas protegidas por senha, ' +
+                      'limpá-los significa que você precisará digitar as senhas novamente.'}
                   </p>
                 </div>
 
@@ -169,14 +168,14 @@ const Navbar = () => {
                     className="mr-3 inline-flex items-center justify-center space-x-2 rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300"
                     onClick={() => setIsOpen(false)}
                   >
-                    {'Cancel'}
+                    {'Cancelar'}
                   </button>
                   <button
                     className="inline-flex items-center justify-center space-x-2 rounded bg-red-500 px-4 py-2 text-white hover:bg-red-400 focus:outline-none focus:ring focus:ring-red-300"
                     onClick={() => clearTokens()}
                   >
                     <FontAwesomeIcon icon={['far', 'trash-alt']} />
-                    <span>{'Clear all'}</span>
+                    <span>{'Limpar todos'}</span>
                   </button>
                 </div>
               </div>
