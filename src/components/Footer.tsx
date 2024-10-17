d5556554990c132933ab0fea48bfa78bb8f9f6fb
wellingto198 <52017348+wellingto198@users.noreply.github.com>
2024-10-17 19:04:04 -0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -16,6 +16,7 @@ const Footer = () => {
   return (
     <div
       className="relative w-full border-t border-gray-900/10 p-4 text-center text-xs font-medium text-gray-400 dark:border-gray-500/30"
+      style={{ minHeight: '100px', paddingBottom: '40px' }}
       dangerouslySetInnerHTML={createFooterMarkup()}
     ></div>
   )
