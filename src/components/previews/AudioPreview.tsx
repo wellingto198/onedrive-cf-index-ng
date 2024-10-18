cc2f550057197edc470b1ab2e7b4c2dab1fc39e7
wellingto198 <52017348+wellingto198@users.noreply.github.com>
2024-10-18 11:53:34 -0300

    fix

--- src/components/previews/AudioPreview.tsx
+++ src/components/previews/AudioPreview.tsx
@@ -1,8 +1,13 @@
+import type { OdFileObject } from '../../types' // Importação correta do tipo
 import { FC, useEffect, useRef, useState } from 'react'
+
 import ReactAudioPlayer from 'react-audio-player'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlay, faPause, faVolumeUp, faVolumeMute, faDownload } from '@fortawesome/free-solid-svg-icons'
 import { useRouter } from 'next/router'
+
+import DownloadButtonGroup from '../DownloadBtnGtoup'
+import { DownloadBtnContainer, PreviewContainer } from './Containers'
+import { LoadingIcon } from '../Loading'
 import { formatModifiedDateTime } from '../../utils/fileDetails'
 import { getStoredToken } from '../../utils/protectedRouteHandler'
 
@@ -16,77 +21,77 @@ enum PlayerState {
 const AudioPreview: FC<{ file: OdFileObject }> = ({ file }) => {
   const { asPath } = useRouter()
   const hashedToken = getStoredToken(asPath)
-  const rapRef = useRef<ReactAudioPlayer>(null)
 
+  const rapRef = useRef<ReactAudioPlayer>(null)
   const [playerStatus, setPlayerStatus] = useState(PlayerState.Loading)
   const [playerVolume, setPlayerVolume] = useState(1)
-  const [isMuted, setIsMuted] = useState(false)
 
+  // Render audio thumbnail, and also check for broken thumbnails
   const thumbnail = `/api/thumbnail?path=${asPath}&size=medium${hashedToken ? `&odpt=${hashedToken}` : ''}`
   const [brokenThumbnail, setBrokenThumbnail] = useState(false)
 
   useEffect(() => {
+    // Manually get the HTML audio element and set onplaying event.
+    // - As the default event callbacks provided by the React component does not guarantee playing state to be set
+    // - properly when the user seeks through the timeline or the audio is buffered.
     const rap = rapRef.current?.audioEl.current
     if (rap) {
       rap.oncanplay = () => setPlayerStatus(PlayerState.Ready)
       rap.onended = () => setPlayerStatus(PlayerState.Paused)
       rap.onpause = () => setPlayerStatus(PlayerState.Paused)
       rap.onplay = () => setPlayerStatus(PlayerState.Playing)
+      rap.onplaying = () => setPlayerStatus(PlayerState.Playing)
+      rap.onseeking = () => setPlayerStatus(PlayerState.Loading)
+      rap.onwaiting = () => setPlayerStatus(PlayerState.Loading)
+      rap.onerror = () => setPlayerStatus(PlayerState.Paused)
       rap.onvolumechange = () => setPlayerVolume(rap.volume)
     }
   }, [])
 
-  const toggleMute = () => {
-    const rap = rapRef.current?.audioEl.current
-    if (rap) {
-      rap.muted = !rap.muted
-      setIsMuted(rap.muted)
-    }
-  }
-
   return (
-    <div className="p-4 bg-white rounded-lg shadow-md dark:bg-gray-800">
-      <div className="flex flex-col space-y-4 md:flex-row md:space-x-4">
-        <div className="relative flex aspect-[2/1] w-full items-center justify-center rounded-full bg-gray-100 transition-all duration-75 dark:bg-gray-700 md:w-48">
-          <div
-            className={`absolute z-20 flex h-full w-full items-center justify-center transition-all duration-300 ${
-              playerStatus === PlayerState.Loading
-                ? 'bg-white opacity-80 dark:bg-gray-800'
-                : 'bg-transparent opacity-0'
-            }`}
-          >
-            <FontAwesomeIcon className="z-10 inline-block h-5 w-5 animate-spin" icon={faPlay} />
-          </div>
+    <>
+      <PreviewContainer>
+        <div className="flex flex-col space-y-4 md:flex-row md:space-x-4">
+          <div className="relative flex aspect-square w-full items-center justify-center rounded bg-gray-100 transition-all duration-75 dark:bg-gray-700 md:w-48">
+            <div
+              className={`absolute z-20 flex h-full w-full items-center justify-center transition-all duration-300 ${
+                playerStatus === PlayerState.Loading
+                  ? 'bg-white opacity-80 dark:bg-gray-800'
+                  : 'bg-transparent opacity-0'
+              }`}
+            >
+              <LoadingIcon className="z-10 inline-block h-5 w-5 animate-spin" />
+            </div>
 
-          {!brokenThumbnail ? (
-            <div className="absolute m-4 aspect-[2/1] w-full rounded-full shadow-lg">
-              <img
-                className={`h-full w-full rounded-full object-cover ${
-                  playerStatus === PlayerState.Playing ? 'animate-spin-slow' : ''
-                }`}
-                src={thumbnail}
-                alt={file.name}
-                onError={() => setBrokenThumbnail(true)}
+            {!brokenThumbnail ? (
+              <div className="absolute m-4 aspect-square rounded-full shadow-lg">
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img
+                  className={`h-full w-full rounded-full object-cover object-top ${
+                    playerStatus === PlayerState.Playing ? 'animate-spin-slow' : ''
+                  }`}
+                  src={thumbnail}
+                  alt={file.name}
+                  onError={() => setBrokenThumbnail(true)}
+                />
+              </div>
+            ) : (
+              <FontAwesomeIcon
+                className={`z-10 h-5 w-5 ${playerStatus === PlayerState.Playing ? 'animate-spin' : ''}`}
+                icon="music"
+                size="2x"
               />
-            </div>
-          ) : (
-            <FontAwesomeIcon
-              className={`z-10 h-5 w-5 ${playerStatus === PlayerState.Playing ? 'animate-spin' : ''}`}
-              icon="music"
-              size="2x"
-            />
-          )}
-        </div>
-
-        <div className="flex flex-col w-full justify-between">
-          <div>
-            <div className="mb-2 text-lg font-medium">{file.name}</div>
-            <div className="mb-4 text-sm text-gray-500">
-              {'Last modified: ' + formatModifiedDateTime(file.lastModifiedDateTime)}
-            </div>
+            )}
           </div>
 
-          <div className="flex items-center space-x-4">
+          <div className="flex w-full flex-col justify-between">
+            <div>
+              <div className="mb-2 font-medium">{file.name}</div>
+              <div className="mb-4 text-sm text-gray-500">
+                {'Last modified:' + ' ' + formatModifiedDateTime(file.lastModifiedDateTime)}
+              </div>
+            </div>
+
             <ReactAudioPlayer
               className="h-11 w-full"
               src={`/api/raw?path=${asPath}${hashedToken ? `&odpt=${hashedToken}` : ''}`}
@@ -94,27 +99,15 @@ const AudioPreview: FC<{ file: OdFileObject }> = ({ file }) => {
               controls
               preload="auto"
               volume={playerVolume}
-              onPlay={() => setPlayerStatus(PlayerState.Playing)}
-              onPause={() => setPlayerStatus(PlayerState.Paused)}
             />
-            <button
-              className="flex items-center justify-center p-2 bg-gray-200 rounded-full dark:bg-gray-600 hover:bg-gray-300 dark:hover:bg-gray-700 transition-all"
-              onClick={toggleMute}
-            >
-              <FontAwesomeIcon icon={isMuted ? faVolumeMute : faVolumeUp} />
-            </button>
           </div>
         </div>
-      </div>
+      </PreviewContainer>
 
-      <div className="mt-4 flex justify-end">
-        <a href={`/api/raw?path=${asPath}${hashedToken ? `&odpt=${hashedToken}` : ''}`} download>
-          <button className="flex items-center px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-all">
-            <FontAwesomeIcon icon={faDownload} className="mr-2" /> Download
-          </button>
-        </a>
-      </div>
-    </div>
+      <DownloadBtnContainer>
+        <DownloadButtonGroup />
+      </DownloadBtnContainer>
+    </>
   )
 }
 
