4fe18b584e15ae1220dfed007f37e3b678292465
wellingto198 <52017348+wellingto198@users.noreply.github.com>
2024-10-24 13:37:04 -0300

    Update VideoPreview.tsx

--- src/components/previews/VideoPreview.tsx
+++ src/components/previews/VideoPreview.tsx
@@ -1,114 +1,112 @@
-import type { OdFileObject } from '../../types'
+import type { OdFileObject } from '../../types';
 
-import { FC, useEffect, useState } from 'react'
-import { useRouter } from 'next/router'
+import { FC, useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 
-import axios from 'axios'
-import toast from 'react-hot-toast'
-import Plyr from 'plyr-react'
-import { useAsync } from 'react-async-hook'
-import { useClipboard } from 'use-clipboard-copy'
+import axios from 'axios';
+import toast from 'react-hot-toast';
+import Plyr from 'plyr-react';
+import { useAsync } from 'react-async-hook';
+import { useClipboard } from 'use-clipboard-copy';
 
-import { getBaseUrl } from '../../utils/getBaseUrl'
-import { getExtension } from '../../utils/getFileIcon'
-import { getStoredToken } from '../../utils/protectedRouteHandler'
+import { getBaseUrl } from '../../utils/getBaseUrl';
+import { getExtension } from '../../utils/getFileIcon';
+import { getStoredToken } from '../../utils/protectedRouteHandler';
 
-import { DownloadButton } from '../DownloadBtnGtoup'
-import { DownloadBtnContainer, PreviewContainer } from './Containers'
-import FourOhFour from '../FourOhFour'
-import Loading from '../Loading'
-import CustomEmbedLinkMenu from '../CustomEmbedLinkMenu'
+import { DownloadButton } from '../DownloadBtnGtoup';
+import { DownloadBtnContainer, PreviewContainer } from './Containers';
+import FourOhFour from '../FourOhFour';
+import Loading from '../Loading';
+import CustomEmbedLinkMenu from '../CustomEmbedLinkMenu';
 
-import 'plyr-react/plyr.css'
+import 'plyr-react/plyr.css';
 
 const VideoPlayer: FC<{
-  videoName: string
-  videoUrl: string
-  width?: number
-  height?: number
-  thumbnail: string
-  subtitle: string
-  isFlv: boolean
-  mpegts: any
+  videoName: string;
+  videoUrl: string;
+  width?: number;
+  height?: number;
+  thumbnail: string;
+  subtitle: string;
+  isFlv: boolean;
+  mpegts: any;
 }> = ({ videoName, videoUrl, width, height, thumbnail, subtitle, isFlv, mpegts }) => {
-  
+
   useEffect(() => {
     // Carregar a legenda automaticamente quando disponível
     axios
       .get(subtitle, { responseType: 'blob' })
       .then(resp => {
-        const track = document.querySelector('track')
+        const track = document.querySelector('track');
         if (track) {
-          track.setAttribute('src', URL.createObjectURL(resp.data))
-          track.setAttribute('default', 'true') // Garantir que a legenda seja padrão
-          const video = document.querySelector('video')
+          track.setAttribute('src', URL.createObjectURL(resp.data));
+          track.setAttribute('default', 'true'); // Garantir que a legenda seja padrão
+          const video = document.querySelector('video');
           if (video) {
-            video.textTracks[0].mode = 'showing' // Forçar a exibição automática das legendas
+            video.textTracks[0].mode = 'showing'; // Forçar a exibição automática das legendas
           }
         }
       })
       .catch(() => {
-        console.log('Could not load subtitle.')
-      })
+        console.log('Could not load subtitle.');
+      });
 
     if (isFlv) {
       const loadFlv = () => {
-        const video = document.getElementById('plyr')
-        const flv = mpegts.createPlayer({ url: videoUrl, type: 'flv' })
-        flv.attachMediaElement(video)
-        flv.load()
-      }
-      loadFlv()
+        const video = document.getElementById('plyr') as HTMLVideoElement;
+        const flv = mpegts.createPlayer({ url: videoUrl, type: 'flv' });
+        flv.attachMediaElement(video);
+        flv.load();
+      };
+      loadFlv();
     }
-  }, [videoUrl, isFlv, mpegts, subtitle])
+  }, [videoUrl, isFlv, mpegts, subtitle]);
 
   const plyrSource = {
     type: 'video',
     title: videoName,
     poster: thumbnail,
     tracks: [{ kind: 'captions', label: videoName, src: '', default: true }],
-  }
+  };
 
-  const plyrOptions: Plyr.Options = {
+  const plyrOptions = {
     ratio: `${width ?? 16}:${height ?? 9}`,
     fullscreen: { iosNative: true },
     captions: { active: true, update: true },
     controls: ['play', 'current-time', 'progress', 'duration', 'captions', 'fullscreen'],
-  }
+  };
 
   if (!isFlv) {
-    plyrSource['sources'] = [{ src: videoUrl }]
+    plyrSource['sources'] = [{ src: videoUrl }];
   }
-  return <Plyr id="plyr" source={plyrSource as Plyr.SourceInfo} options={plyrOptions} />
-}
 
- return (
+  return (
     <div>
       <h2 style={{ textAlign: 'center', marginBottom: '10px' }}>{videoName}</h2> {/* Adiciona o título acima do player */}
-      <Plyr id="plyr" source={plyrSource as Plyr.SourceInfo} options={plyrOptions} />
+      <Plyr id="plyr" source={plyrSource} options={plyrOptions} />
     </div>
-  )
-}
+  );
+};
 
 const VideoPreview: FC<{ file: OdFileObject }> = ({ file }) => {
-  const { asPath } = useRouter()
-  const hashedToken = getStoredToken(asPath)
-  const clipboard = useClipboard()
+  const { asPath } = useRouter();
+  const hashedToken = getStoredToken(asPath);
+  const clipboard = useClipboard();
 
-  const [menuOpen, setMenuOpen] = useState(false)
-  const [legendasDisponiveis, setLegendasDisponiveis] = useState(true) // Inicialmente assume que legendas estão disponíveis
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [legendasDisponiveis, setLegendasDisponiveis] = useState(true); // Inicialmente assume que legendas estão disponíveis
 
-  const thumbnail = `/api/thumbnail?path=${asPath}&size=large${hashedToken ? `&odpt=${hashedToken}` : ''}`
-  const vtt = `${asPath.substring(0, asPath.lastIndexOf('.'))}.vtt`
-  const subtitle = `/api/raw?path=${vtt}${hashedToken ? `&odpt=${hashedToken}` : ''}`
-  const videoUrl = `/api/raw?path=${asPath}${hashedToken ? `&odpt=${hashedToken}` : ''}`
+  const thumbnail = `/api/thumbnail?path=${asPath}&size=large${hashedToken ? `&odpt=${hashedToken}` : ''}`;
+  const vtt = `${asPath.substring(0, asPath.lastIndexOf('.'))}.vtt`;
+  const subtitle = `/api/raw?path=${vtt}${hashedToken ? `&odpt=${hashedToken}` : ''}`;
+  const videoUrl = `/api/raw?path=${asPath}${hashedToken ? `&odpt=${hashedToken}` : ''}`;
 
-  const isFlv = getExtension(file.name) === 'flv'
+  const isFlv = getExtension(file.name) === 'flv';
   const { loading, error, result: mpegts } = useAsync(async () => {
     if (isFlv) {
-      return (await import('mpegts.js')).default
+      return (await import('mpegts.js')).default;
     }
-  }, [isFlv])
+  }, [isFlv]);
 
   // Função para verificar a existência das legendas
   const verificarExistenciaLegendas = async (subtitleUrl: string) => {
@@ -147,7 +145,7 @@ const VideoPreview: FC<{ file: OdFileObject }> = ({ file }) => {
             mpegts={mpegts}
           />
         )}
-        
+
         <p
           style={{
             textAlign: 'center',
@@ -171,8 +169,8 @@ const VideoPreview: FC<{ file: OdFileObject }> = ({ file }) => {
           />
           <DownloadButton
             onClickCallback={() => {
-              clipboard.copy(`${getBaseUrl()}/api/raw?path=${asPath}${hashedToken ? `&odpt=${hashedToken}` : ''}`)
-              toast.success('Link Copiado.')
+              clipboard.copy(`${getBaseUrl()}/api/raw?path=${asPath}${hashedToken ? `&odpt=${hashedToken}` : ''}`);
+              toast.success('Link Copiado.');
             }}
             btnColor="pink"
             btnText={'Copiar Link'}
@@ -209,65 +207,60 @@ const VideoPreview: FC<{ file: OdFileObject }> = ({ file }) => {
           )}
         </p>
 
-        <div style={{ marginBottom: '20px' }} />
+        <div className="flex justify-center gap-4">
+          <DownloadButton
+            onClickCallback={() => {
+              const videoUrl = `${getBaseUrl().replace(/\/$/, '')}/api/raw?path=${asPath}${hashedToken ? `&odpt=${hashedToken}` : ''}`;
+              const encodedUrl = encodeURIComponent(videoUrl); // Codifica a URL
+              window.location.href = `iina://weblink?url=${encodedUrl}`;
+            }}
+            btnText="IINA"
+            btnImage="/players/iina.png"
+          />
 
-        {!isWindows && (
-          <>
-            <DownloadButton
-  onClickCallback={() => {
-    const videoUrl = `${getBaseUrl().replace(/\/$/, '')}/api/raw?path=${asPath}${hashedToken ? `&odpt=${hashedToken}` : ''}`;
-    const encodedUrl = encodeURIComponent(videoUrl); // Codifica a URL
-    window.location.href = `iina://weblink?url=${encodedUrl}`;
-  }}
-  btnText="IINA"
-  btnImage="/players/iina.png"
-/>
+          <DownloadButton
+            onClickCallback={() => {
+              const videoUrl = `${getBaseUrl().replace(/\/$/, '')}/api/raw?path=${asPath}${hashedToken ? `&odpt=${hashedToken}` : ''}`;
+              const vlcUrl = `vlc://${videoUrl.replace(/^https?:\/\//, '')}`; // Remove protocolo http/https
+              window.location.href = vlcUrl;
+            }}
+            btnText="VLC"
+            btnImage="/players/vlc.png"
+          />
 
-<DownloadButton
-  onClickCallback={() => {
-    const videoUrl = `${getBaseUrl().replace(/\/$/, '')}/api/raw?path=${asPath}${hashedToken ? `&odpt=${hashedToken}` : ''}`;
-    const vlcUrl = `vlc://${videoUrl.replace(/^https?:\/\//, '')}`; // Remove protocolo http/https
-    window.location.href = vlcUrl;
-  }}
-  btnText="VLC"
-  btnImage="/players/vlc.png"
-/>
+          <DownloadButton
+            onClickCallback={() => {
+              const videoUrl = `${getBaseUrl().replace(/\/$/, '')}/api/raw?path=${asPath}${hashedToken ? `&odpt=${hashedToken}` : ''}`;
+              const potPlayerUrl = `potplayer://${videoUrl.replace(/^https?:\/\//, '')}`; // Remove protocolo http/https
+              window.location.href = potPlayerUrl;
+            }}
+            btnText="PotPlayer"
+            btnImage="/players/potplayer.png"
+          />
 
-<DownloadButton
-  onClickCallback={() => {
-    const videoUrl = `${getBaseUrl().replace(/\/$/, '')}/api/raw?path=${asPath}${hashedToken ? `&odpt=${hashedToken}` : ''}`;
-    const potPlayerUrl = `potplayer://${videoUrl.replace(/^https?:\/\//, '')}`; // Remove protocolo http/https
-    window.location.href = potPlayerUrl;
-  }}
-  btnText="PotPlayer"
-  btnImage="/players/potplayer.png"
-/>
+          <DownloadButton
+            onClickCallback={() => {
+              const videoUrl = `${getBaseUrl().replace(/\/$/, '')}/api/raw?path=${asPath}${hashedToken ? `&odpt=${hashedToken}` : ''}`;
+              const nPlayerUrl = `nplayer://${videoUrl.replace(/^https?:\/\//, '')}`; // Remove protocolo http/https
+              window.location.href = nPlayerUrl;
+            }}
+            btnText="NPlayer"
+            btnImage="/players/nplayer.png"
+          />
 
-<DownloadButton
-  onClickCallback={() => {
-    const videoUrl = `${getBaseUrl().replace(/\/$/, '')}/api/raw?path=${asPath}${hashedToken ? `&odpt=${hashedToken}` : ''}`;
-    const nPlayerUrl = `nplayer-http://${videoUrl.replace(/^https?:\/\//, '')}`; // Remove protocolo http/https
-    window.location.href = nPlayerUrl;
-  }}
-  btnText="nPlayer"
-  btnImage="/players/nplayer.png"
-/>
-
-<DownloadButton
-  onClickCallback={() => {
-    const videoUrl = `${getBaseUrl().replace(/\/$/, '')}/api/raw?path=${asPath}${hashedToken ? `&odpt=${hashedToken}` : ''}`;
-    const mpvUrl = `intent://${videoUrl.replace(/^https?:\/\//, '')}#Intent;type=video/any;package=is.xyz.mpv;scheme=https;end;`; // Remove protocolo http/https
-    window.location.href = mpvUrl;
-  }}
-  btnText="mpv-android"
-  btnImage="/players/mpv-android.png"
-/>
-
-          </>
-        )}
+          <DownloadButton
+            onClickCallback={() => {
+              const videoUrl = `${getBaseUrl().replace(/\/$/, '')}/api/raw?path=${asPath}${hashedToken ? `&odpt=${hashedToken}` : ''}`;
+              const mpvUrl = `mpv://${videoUrl.replace(/^https?:\/\//, '')}`; // Remove protocolo http/https
+              window.location.href = mpvUrl;
+            }}
+            btnText="MPV"
+            btnImage="/players/mpv.png"
+          />
+        </div>
       </DownloadBtnContainer>
     </>
-  )
-}
+  );
+};
 
-export default VideoPreview
+export default VideoPreview;
